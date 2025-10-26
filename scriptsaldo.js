c2ad1bfd4c7166ed05122d62e1c812b002676de2
gabroleu <rgabrielcs@gmail.com>
2025-10-26 03:28:22 -0400

    script que verifica o saldo atual

---
+++ scriptsaldo.js
@@ -0,0 +1,7 @@
+//script que cria o saldo, diminui valor usado e depois mostra o saldo atual
+let saldo = 1000;
+let gasto = 250;
+
+saldo = saldo - gasto;
+
+console.log(`seu saldo atual é de ${saldo}.`);
