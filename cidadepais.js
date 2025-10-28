0ba69a97e44b65c82af6fa586589942d277b2cf2
gabroleu <rgabrielcs@gmail.com>
2025-10-28 19:48:09 -0400

    cidade e pais

---
+++ cidadepais.js
@@ -0,0 +1,8 @@
+//crie uma variável cidade com o nome cidade, crie uma constante país com o valor "Brasil" e 
+//e mostre a frase  MORO EM MANAUS BRASIL (Usando o template strings)
+
+
+let cidade = "Manaus";
+const pais = "Brasil";
+
+console.log(`Moro em ${cidade}, ${pais}.`);
