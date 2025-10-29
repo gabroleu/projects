5437694681d1a0a538e7460f1f11c2c4e69a9c4c
gabroleu <rgabrielcs@gmail.com>
2025-10-28 21:53:53 -0400

    switch case

---
+++ switchCase.js
@@ -0,0 +1,17 @@
+//switch case
+
+let diaDaSemana = 1;
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("Segunda-Feira")
+        break
+
+    case 2:
+        console.log("Terça-Feira")
+        break
+
+    case 3:
+        console.log("Quarta-Feira")
+
+}
