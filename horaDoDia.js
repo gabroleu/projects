f85f2099bd47a017a770aeeb8b911c7b292b513f
gabroleu <rgabrielcs@gmail.com>
2025-10-28 21:41:25 -0400

    hora do dia

---
+++ horaDoDia.js
@@ -0,0 +1,12 @@
+//saudação do dia
+
+let hora = 08;
+
+if (hora <12) {
+    console.log("Bom dia!")
+}else if (hora < 18){
+    console.log("Boa tarde!")
+}else{
+    console.log("Boa noite!")
+}
+
