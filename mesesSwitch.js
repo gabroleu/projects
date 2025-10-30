80e34d6914d2a2cd14df8bc714cf98912fb0224b
gabroleu <rgabrielcs@gmail.com>
2025-10-29 22:35:37 -0400

    Meses do Ano

---
+++ mesesSwitch.js
@@ -0,0 +1,46 @@
+//Use um switch para exibir o nome de um mês com base em um número (1 para Janeiro, 2 para Fevereiro, etc.).
+
+let mes = 6;
+
+switch (mes) {
+    case 1:
+    console.log("janeiro");
+    break;
+    case 2:
+    console.log("fevereiro");
+    break;
+    case 3:
+    console.log("março");
+    break;
+    case 4:
+    console.log("abril");
+    break;
+    case 5:
+    console.log("maio");
+    break;
+    case 6:
+    console.log("junho");
+    break;
+    case 7:
+    console.log("julho");
+    break;
+    case 8:
+    console.log("agosto");
+    break;
+    case 9:
+    console.log("setembro");
+    break;
+    case 10:
+    console.log("outubro");
+    break;
+    case 11:
+    console.log("novembro");
+    break;
+    case 12:
+    console.log("dezembro");
+    break;
+    default:
+        console.log("Número inválido! Digite um valor de 1 a 12.");
+
+
+}
