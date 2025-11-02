dd1ba6c4c6dd7fd12bb3d6bb1ffb16c9b735a221
gabroleu <rgabrielcs@gmail.com>
2025-11-01 23:37:27 -0400

    aprovar compra

---
+++ operadorOu.js
@@ -0,0 +1,10 @@
+// operador || (Ou) - saldo do cartão
+
+let saldo = 1000;
+let cartão = true;
+
+if (saldo > 0 || temCartao) {
+    console.log("Compra aprovada!");
+} else {
+    console.log("Compra negada!")
+}
