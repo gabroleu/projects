b0b8a00fe55c05e6b31fdd29e8dfbb9d0c691b81
gabroleu <rgabrielcs@gmail.com>
2025-11-01 23:08:04 -0400

    Classificando nota

---
+++ classificandoNota.js
@@ -0,0 +1,11 @@
+//classificando notas com &&
+
+let nota = 100;
+
+if (nota >= 90) {
+    console.log("Nota A!");
+} else if (nota >= 70 && nota < 90){
+    console.log("Nota B!");
+} else{
+    console.log("Reprovado");
+}
