47770c20ba9247c5b269709c4a277f93868f2810
gabroleu <rgabrielcs@gmail.com>
2025-11-01 22:42:12 -0400

    Pode dirigir ou não

---
+++ podeDirigirOuNao.js
@@ -0,0 +1,10 @@
+// pode dirigir ou não com Booleanos
+
+let idade = 20;
+let temCarteira = true;
+
+if (idade >= 18 && temCarteira){
+    console.log("Pode Dirigir!");
+} else {
+    console.log("Não pode dirigir!")
+}
