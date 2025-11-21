e26c1d167d2f5e2f8aee8e6ac5227c69066489fa
gabroleu <rgabrielcs@gmail.com>
2025-11-21 19:37:52 -0400

    Loop com For

--- loopFoor.js
+++ loopFoor.js
@@ -8,6 +8,6 @@ for (let i = 0; i < 5; i++){
     console.log (`Numero ${i}`);
 }
 
-// let i = 0: inicializa o contador
-// i < 5: define a condição para continuar o loop
-// i++: incrementa o valor de i a cada iteração
+// let i = 0: inicializa o contador;
+// i < 5: define a condição para continuar o loop;
+// i++: incrementa o valor de i a cada iteração.
