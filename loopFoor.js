5d431bd9224dc752bea60528641bfdc588229c4f
gabroleu <rgabrielcs@gmail.com>
2025-11-21 19:32:37 -0400

    loop com for

---
+++ loopFoor.js
@@ -0,0 +1,13 @@
+//o loop for é usado quando você sabe exatamente quantas vezes deseja repetir o cógigo.
+
+for (let indice = 0; indice < 20; indice++) {
+    console.log("Numero", indice)
+}
+
+for (let i = 0; i < 5; i++){
+    console.log (`Numero ${i}`);
+}
+
+// let i = 0: inicializa o contador
+// i < 5: define a condição para continuar o loop
+// i++: incrementa o valor de i a cada iteração
